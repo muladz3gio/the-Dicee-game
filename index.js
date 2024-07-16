7e1a5329b769ab1336c0d22adbb7c52211c15b13
muladz3gio <153388774+muladz3gio@users.noreply.github.com>
2024-07-16 15:07:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor((Math.random()*6)) + 1;
-document.querySelectorAll("img")[0].setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelectorAll("img")[0].setAttribute("src", "./dice"+randomNumber1+".png");
 var randomNumber2 = Math.floor((Math.random()*6)) + 1;
-document.querySelectorAll("img")[1].setAttribute("src", "./images/dice"+randomNumber2+".png");
+document.querySelectorAll("img")[1].setAttribute("src", "./dice"+randomNumber2+".png");
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player one wins!"
 }else if (randomNumber2 > randomNumber1){
